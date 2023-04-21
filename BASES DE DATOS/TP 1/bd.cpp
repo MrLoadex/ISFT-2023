48ccc6765d09820ee172d71c8f87dfb2959efd97
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-21 17:35:16 -0300

    Add files via upload

---
+++ BASES DE DATOS/TP 1/bd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <AttendanceManagement.hpp>
+#include <Student.hpp>
+#include <Course.hpp>
+
+using namespace std;
+
+int main()
+{
+
+    Student* student = new Student();
+    student->setIdentifier(1);
+    student->setName("Juan");
+    student->setSurname("Perez");
+
+    Course* course = new Course();
+    course->setIdentifier(1);
+    course->setName("Algebra");
+
+    AttendanceManagement* attendanceManagement = new AttendanceManagement();
+    attendanceManagement->TakeAttendance(*student, *course, "2021-06-08", true);
+
+    attendanceManagement->ShowAttendance();
+
+    delete student;
+    delete course;
+    delete attendanceManagement;
+
+    return 0;
+}
+
